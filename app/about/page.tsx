ad8bae989c69559f8f78843cdeba0333dd9675a8
Han-Enc <h01062598017@gmail.com>
2023-05-22 15:25:27 +0900

    3

---
+++ app/about/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'Next13 About',
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Next.js 13</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis est
+        debitis corrupti unde architecto, commodi nemo ducimus suscipit ea ad
+        aspernatur asperiores a cum. Nobis dicta tenetur dolor cum officia!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        maiores ullam quisquam a perspiciatis, saepe vitae deleniti doloremque
+        esse, sed fuga ut laboriosam magni fugiat corporis debitis nam repellat
+        culpa id omnis. Quisquam necessitatibus quo veniam fugit officia maiores
+        odio mollitia nostrum assumenda quas optio, velit, ea quia non rerum?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
