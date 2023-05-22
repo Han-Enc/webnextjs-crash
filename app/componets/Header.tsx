e8d930cbd5d34bf82e0787e74a7bee0042144123
Han-Enc <h01062598017@gmail.com>
2023-05-22 14:51:18 +0900

    2

--- app/componets/Header.tsx
+++ app/componets/Header.tsx
@@ -1,8 +1,23 @@
+import Link from 'next/link';
 import { fromJSON } from 'postcss';
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="logo">
+          <Link href="/">Next13</Link>
+        </div>
+        <div className="links">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/about/team">Team</Link>
+          <Link href="/code/repos">Code</Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
